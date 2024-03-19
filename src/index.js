708c28df6058f6867a5347820528fa82803de305
juliedosher <juliegdosher@lewisu.edu>
2024-03-19 00:17:33 -0500

    removed auth0 from client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { StrictMode, createElement } from "react";
 import { createRoot } from "react-dom/client";
-import { Auth0Provider } from '@auth0/auth0-react';
 import "./styles.css";
 
 import App from "./App";
@@ -9,14 +8,6 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <Auth0Provider
-      domain="dev-dtw5lccxhfi7uzjg.us.auth0.com"
-      clientId="TfYTlnXTHMrwYj8YfXJ76NEP2YoOwxzr"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-    >
-      <App />
-    </Auth0Provider>,
+    <App />
   </StrictMode>
 );
